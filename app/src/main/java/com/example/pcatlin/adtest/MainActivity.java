0181bd91a75c88135ed1e34e0cb298352f19665c
Paul Catlin <pcatlin@lheath.net>
2018-02-13 14:53:37 +0000

    Initial commit

---
+++ app/src/main/java/com/example/pcatlin/adtest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pcatlin.adtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, "ca-app-pub-9612116433207542~5183276592");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+    }
+}
